63f71a59a7b6bad45baba4f9f01a0af51b8660ed
Axel V <axelvalenzuela7@gmail.com>
2021-01-19 15:24:30 -0600

    Funcion de buscar funciona, agregue funcion para poder diferenciar las respuestas correctas de las malas

--- lib/main.dart
+++ lib/main.dart
@@ -56,27 +56,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -87,66 +73,62 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            _searchBar(),
             new Flexible(
               child: new FirebaseAnimatedList(
                   query: itemRefShop,
                   itemBuilder: (_, DataSnapshot snapshot,
                       Animation<double> animation, int index) {
-                    return new ListTile(
-                      title: new Text(snapshot.value['answer']),
+                    return index == 0
+                        ? _searchBar()
+                        :  new ListTile(
+                      title: new Text(
+                          'Usuario: ' + notaMostrada[index-1].userId),
+                      subtitle: new Text('Respuesta: ' +
+                          itemsShop[index].answer +
+                          '\n' +
+                          'Id de pregunta: ' +
+                          itemsShop[index].questionId),
+                      leading: itemsShop[index].right == false
+                          ? _icono()
+                          : _icono(),
                     );
                   }),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 
-  _searchBar(){
+  _icono() {
+    return Icon(
+      Icons.ac_unit_rounded,
+      size: 10,
+    );
+  }
+  _searchBar() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextField(
-        decoration: InputDecoration(
-          hintText: 'Search...'
-        ),
-      onChanged: (text){
-          text=text.toLowerCase();
-      },
+        decoration: InputDecoration(hintText: 'Search...'),
+        onChanged: (text) {
+          text = text.toLowerCase();
+          setState(() {
+            notaMostrada = itemsShop.where((note) {
+              var noteTitle = note.userId.toLowerCase();
+              return noteTitle.contains(text);
+            }).toList();
+          });
+        },
       ),
     );
   }
 
   List<Shop> itemsShop = List();
+  List<Shop> notaMostrada = List<Shop>();
   Shop itemShop;
   DatabaseReference itemRefShop;
 
